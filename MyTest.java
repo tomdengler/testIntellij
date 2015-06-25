f24faeafa9959ed9a7d07bb112efaee0b724a17f
Tom <tonmdengler@gmail.com>
2015-06-24 23:12:37 -0400

    user.email fixed

--- MyTest.java
+++ MyTest.java
@@ -3,6 +3,6 @@
  */
 public class MyTest{
     public static void main(String[] args) {
-        System.out.println("Hello world! bubba was ptobsby not here, was commited locally again");
+        System.out.println("Hello world! bubba was ptobsby not here, was commited locally again, and again");
     }
 }
